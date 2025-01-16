4057c4ed71e0ec7a989654e1a87ed63a6cd5ed70
Devlin <devlin@devlinrocha.com>
2025-01-15 16:22:11 -0800

    fix method

--- run_inference.py
+++ run_inference.py
@@ -6,7 +6,7 @@ MODULE_NAME = "lilypad-module"
 MODEL="distilbert-base-uncased-finetuned-sst-2-english"
 
 def main():
-    text = os.environment.get('INPUT_TEXT', 'Default text for analysis')
+    text = os.environ.get('INPUT_TEXT', 'Default text for analysis')
 
     classifier = pipeline(MODULE_NAME, model=MODEL, device=-1)
 
@@ -37,4 +37,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-

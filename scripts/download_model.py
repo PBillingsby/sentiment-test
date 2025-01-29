ab2cceb4774161487c7fb2f9f981cd52fd111c6e
Devlin <devlin@devlinrocha.com>
2025-01-29 15:28:46 -0800

    use `Auto`

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -1,10 +1,10 @@
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
 def download_model():
     model_name = "distilbert-base-uncased-finetuned-sst-2-english"
-    tokenizer = DistilBertTokenizer.from_pretrained(model_name)
-    model = DistilBertForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
     # Save the tokenizer and model
     tokenizer.save_pretrained("./models")
